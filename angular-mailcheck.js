4b6cfc13dca0d2192246d3c6147764272e1f26de
lmelinda <melinda.lu46@gmail.com>
2018-05-24 15:22:16 -0400

    Remove email's hyperlink, and nope text.

--- angular-mailcheck.js
+++ angular-mailcheck.js
@@ -87,8 +87,7 @@
         var templateHtml =
           '<div class="help-block mailcheck" ng-show="suggestion && !bugmenot">' +
             '{{ suggestionText }} ' +
-            '<a ng-bind="suggestion" ng-click="useSuggestion()"></a>? ' +
-            '<a ng-click="suggestion=false; bugmenot=true">{{ nopeText }}</a>' +
+            '<span ng-bind="suggestion"></span>? ' +
           '</div>';
 
         // Compiled template
